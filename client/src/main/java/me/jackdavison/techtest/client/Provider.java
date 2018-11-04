3710f55d29d6487cfc3c98755594de70c24ff0bd
Jack Davison <jlogandavison@gmail.com>
2018-11-04 03:34:51 +0000

    Use List instead of ArrayList in Provider interface

--- client/src/main/java/me/jackdavison/techtest/client/Provider.java
+++ client/src/main/java/me/jackdavison/techtest/client/Provider.java
@@ -1,11 +1,11 @@
 package me.jackdavison.techtest.client;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import me.jackdavison.techtest.client.types.Location;
 import me.jackdavison.techtest.client.types.Ride;
 
 public interface Provider {
-    public ArrayList<Ride> getRides(Location pickup, Location destination);
+    public List<Ride> getRides(Location pickup, Location destination);
 }
 
