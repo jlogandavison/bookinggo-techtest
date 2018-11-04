8133d37a373f13a85c92e7a312ee1a160008d5ff
Jack Davison <jlogandavison@gmail.com>
2018-11-04 15:42:25 +0000

    Get REST controller to return result from provider (just like commandline)

--- rest/src/main/java/me/jackdavison/techtest/rest/RideSearchController.java
+++ rest/src/main/java/me/jackdavison/techtest/rest/RideSearchController.java
@@ -1,14 +1,43 @@
 package me.jackdavison.techtest.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import me.jackdavison.techtest.client.types.Location;
+import me.jackdavison.techtest.client.types.Ride;
+import me.jackdavison.techtest.client.HttpApiProvider;
+import me.jackdavison.techtest.client.Provider;
 
 @RestController
 public class RideSearchController {
 
-    @GetMapping("/")
-    String helloworld() {
-        return "Hello World";
+    private final Provider provider;
+
+    RideSearchController() throws URISyntaxException {
+        this.provider = new HttpApiProvider(new URI("https://techtest.rideways.com/dave/"));
     }
+
+    @GetMapping("/")
+    List<Ride> getRides(
+            @RequestParam(value="pickup", required=true) String str_pickup,
+            @RequestParam(value="dropoff", required=true) String str_dropoff) {
+
+        Location pickup = Location.parseLocation(str_pickup);
+        Location dropoff = Location.parseLocation(str_dropoff);
+
+        // Get rides from provider(s)
+        List<Ride> rides = provider.getRides(pickup, dropoff);
+
+        // Sort output by price
+        Collections.sort(rides, new Ride.PriceComparator());
+        return rides;
+    }
+
 }
 
